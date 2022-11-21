f11894aa3ce2d970a119217cfc4be2d601dec454
Maricel Garcia <118769671+zamaricel@users.noreply.github.com>
2022-11-21 15:29:44 -0500

    Update streamlit_app.py
    Updated menu with correct syntax based on instructions from snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.('Breakfast Menu')
-('Omega 3')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach, & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
