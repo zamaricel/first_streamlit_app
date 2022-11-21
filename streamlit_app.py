6b724f4c1304b1fd8431615c1c3ec63a87e15ebc
Maricel Garcia <118769671+zamaricel@users.noreply.github.com>
2022-11-21 15:33:30 -0500

    Update streamlit_app.py
    Added streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach, & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Acovacdo Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
